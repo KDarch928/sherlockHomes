b99a84c975b26605bf08c113d2405fa921350524
Luke Stahl <Stahl1109@gmail.com>
2018-03-31 08:36:29 -0500

    css

--- config/connection.js
+++ config/connection.js
@@ -17,8 +17,8 @@ if (process.env.JAWSDB_URL){
         port: 3306,
         host: "localhost",
         user: "root",
-        // password: key.mysqlaccess.access,
-        passqord: "",
+        password: key.mysqlaccess.access,
+        // passqord: "",
         database: "home_db"
     });
 };
